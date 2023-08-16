c8df0f17a2e5040e38c04fd95a02ea3076e38112
arun raj <113658607+arun-261@users.noreply.github.com>
2023-08-16 12:00:14 +0530

    Shop-nike

---
+++ src/Components/Content.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Content() {
+  return (
+    
+         <main className='content container'>
+   <div className="content-heading">
+    <h1>YOUR FEET DESERVE THE BEST</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure provident doloribus blanditiis
+         magni officia, animi voluptat</p>
+  <div className='content-btn'>
+   <button>Shop Now</button>
+   <button className='secondary'>Category</button>
+  </div>
+
+  <div className="Shopping">
+    <p>Also avoilable on</p>
+
+
+    <div className="Brandicons">
+        <img src="https://www.freepnglogos.com/uploads/flipkart-logo-png/flipkart-logo-transparent-png-download-0.png" alt="" />
+        <img src="https://www.pngmart.com/files/Amazon-Logo-PNG-Image.png" alt="" />
+    </div>
+  </div>
+   </div>
+   <div className="content-img">
+    <img src="https://freepngimg.com/save/28530-nike-shoes-transparent/1464x1533" alt="" />
+   </div>
+   </main>
+    
+  
+  )
+}
+
+export default Content
---
+++ src/Components/Navbar1.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+function Navbar1() {
+  return (
+    <div className='container'>
+       <nav>
+        <div className="logo">
+            <img src="https://freepngimg.com/thumb/nike_logo/6-2-nike-logo-free-png-image.png" alt="no image" />
+        </div>
+        <ul>
+            <li href="#">Menu</li>
+            <li href="#">Location</li>
+            <li href="#">About</li>
+            <li href="#">Contact</li>
+        </ul>
+        <button>Login</button>
+       </nav>
+    </div>
+  )
+}
+
+export default Navbar1
